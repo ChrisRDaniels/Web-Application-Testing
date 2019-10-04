c4f1b4759c3fc16dba6e8f95f64796d8cfc24606
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-10-04 09:16:34 -0600

    commit before starting sprint

--- web-application-testing/src/App.test.js
+++ web-application-testing/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from "@testing-library/react"
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+test('renders without crashing', () => {
+  render(<App />, );
 });
+
+test('strikes display is found', () => {
+  const { getByTestId } = render(<App />);
+
+  getByTestId(/strikes-display/i)
+})
+
+test('balls display is found', () => {
+  const { getByText } = render(<App />)
+
+  getByText(/balls/i)
+})
